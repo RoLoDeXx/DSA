a51a1c17c648594fb98b7c31717cfadeef79aeb2
RoLoDeXx <rolodexx@gitlab.com>
2020-04-22 03:46:43 +0530

    add method

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -1,11 +1,25 @@
-let node = {
-  head: null,
-  value: null,
-};
-
-class LinkedList {
+class Node {
   constructor(value) {
     this.value = value;
     this.next = null;
   }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = null;
+  }
+
+  add = (value) => {
+    let node = new Node(value);
+    if (this.head === null) this.head = node;
+    else {
+      current = this.head;
+      while (current.next) current = current.next;
+
+      current.next = node;
+    }
+    this.size++;
+  };
 }
